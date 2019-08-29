31e05385e08b371d395fdb5742c3ff000bfd908d
jeni <s7130457@gmail.com>
2019-08-29 14:36:45 +0800

    feat(init): 增加ＥＳ６的設定、ＤＢ增家log table

--- init.sql
+++ init.sql
@@ -11,3 +11,12 @@ CREATE TABLE IF NOT EXISTS public.user (
 INSERT INTO public.user (username, email, password, role) 
   VALUES ('admin', 'admin', '$2a$10$r1gEj1Zbko0enyW346mzbu5FWa0VfsXmSE8h0vM4HVCT6pAvz3KLm', 'admin')
   ON CONFLICT (username) DO NOTHING;
+
+CREATE TABLE IF NOT EXISTS public.log (
+  logUid SERIAL PRIMARY KEY, 
+  type TEXT CHECK( type IN ('info', 'warning', 'error', 'alert') ), 
+  category TEXT CHECK( category IN ('auth', 'action', 'device', 'event') ), 
+  "user" TEXT default NULL, 
+  msg TEXT NOT NULL,
+  createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router();
+const httpStatus = require('http-status')
 
+import authRoutes from './auth.route'
+import logRoutes from './log.route'
+import userRoutes from './user.route'
 
 router.get('/', function(req, res) {
   console.log('hi');
@@ -9,6 +13,21 @@ router.get('/', function(req, res) {
 })
 
 
+
+
+router.use('/auth', authRoutes)
+router.use('/logs', checkAuth, logRoutes)
+router.use('/users', checkAuth, userRoutes)
+
+function checkAuth (req, res, next) {
+    // next()
+  if (!req.session || !(req.session.authenticated)) {
+    return res.json(httpStatus.UNAUTHORIZED, {message: 'Authentication error'})
+    // next(new APIError('Authentication error', httpStatus.UNAUTHORIZED, true))
+  } else {
+    next()
+  }
+}
 
 
 module.exports = router
